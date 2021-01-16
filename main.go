1d7623db20d74af5e8d03a76c2ad90fffe44f29d
cristian <cristianmr.28@gmail.com>
2021-01-15 18:53:41 -0600

    feat: prepare history

--- main.go
+++ main.go
@@ -12,14 +12,26 @@ func root(w http.ResponseWriter, r *http.Request) {
 }
 
 func summary(w http.ResponseWriter, r *http.Request) {
+	// TODO: Next step is to fetch Current data from an API
 	fmt.Fprintf(w, "This is today's currency values:\n")
-	fmt.Fprintf(w, fmt.Sprint(print(entries[0])))
+	fmt.Fprintf(w, fmt.Sprint(print(entries[len(entries)-1])))
+	fmt.Println("Endpoint Hit: /summary")
+}
+
+func history(w http.ResponseWriter, r *http.Request) {
+	// TODO: Next step is to fetch Current data from an API
+	fmt.Fprintf(w, "Last "+fmt.Sprint(len(entries))+" days' currency values:\n")
+
+	for i := 0; i < len(entries); i++ {
+		fmt.Fprintf(w, fmt.Sprint(print(entries[len(entries)-i-1])))
+	}
 	fmt.Println("Endpoint Hit: /summary")
 }
 
 func handleRequests() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/summary", summary)
+	http.HandleFunc("/history", history)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
@@ -36,7 +48,10 @@ func print(this entry) string {
 var entries []entry
 
 func main() {
-	// This is a default value
-	entries = []entry{entry{1.0, 0.83, 611.59}}
+	// This is an initialization with static data
+	entries = []entry{
+		entry{1.0, 0.82, 613.27},
+		entry{1.0, 0.83, 611.59},
+	}
 	handleRequests()
 }
